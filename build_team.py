1e69ccb2a92441095b6c5f66f8b26a156df41f4b
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-10-25 13:50:17 +0100

    create file for creating a team of pokemon

---
+++ build_team.py
@@ -0,0 +1,9 @@
+"""Handles all functions related to building a team selected pokemon"""
+
+
+class Pokemon_Team():
+    """"""
+
+    def __init__(self, team_name: str,) -> None:
+        self._team_name = team_name
+        self._current_team = []
