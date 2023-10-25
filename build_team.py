f0bbc76a9cbb8c0c3bf9979f73707816ec5cc922
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-10-25 14:19:16 +0100

    Add tests for creating pokemon team name

--- build_team.py
+++ build_team.py
@@ -1,9 +1,37 @@
 """Handles all functions related to building a team selected pokemon"""
 
+import re
+
 
 class Pokemon_Team():
-    """"""
+    """Class for creating a team of pokemon"""
 
     def __init__(self, team_name: str,) -> None:
+
+        # TODO figure out how to move edge cases into setter
+        if not isinstance(team_name, str):
+            raise TypeError("Error: Team nam must be a string!")
+
+        if len(team_name) < 4:
+            raise ValueError(
+                "Error: Team name must be longer than 4 characters!")
+
+        if len(team_name) > 20:
+            raise ValueError(
+                "Error: Team name must be shorter than 20 characters!")
+
+        if len(team_name) == team_name.count(" "):
+            raise ValueError("Error: Team name cannot be empty spaces!")
+
         self._team_name = team_name
         self._current_team = []
+
+    @property
+    def team_name(self) -> str:
+        """Returns team name"""
+        return self._team_name
+
+    @team_name.setter
+    def team_name(self, value: str) -> None:
+
+        self._team_name = value
---
+++ test_build_team.py
@@ -0,0 +1,40 @@
+"""Testing file for build_team"""
+
+import pytest
+
+from build_team import Pokemon_Team
+
+
+def test_class_pokemon_team_invalid_name():
+    """Tests exception raised for creating a team name with just a number"""
+
+    with pytest.raises(Exception):
+        Pokemon_Team(10)
+
+
+def test_class_pokemon_team_invalid_name_too_short():
+    """Tests exception is raised if team name is under 4 characters"""
+
+    with pytest.raises(Exception):
+        Pokemon_Team("te")
+
+
+def test_class_pokemon_team_invalid_name_too_long():
+    """Tests exception is raised if team name is over 20 characters"""
+
+    with pytest.raises(Exception):
+        Pokemon_Team("This name is definitely too long for a team name!")
+
+
+def test_class_pokemon_team_invalid_name_empty_spaces():
+    """Tests exception is raised if team name is empty spaces"""
+
+    with pytest.raises(Exception):
+        Pokemon_Team("      ")
+
+
+def test_class_pokemon_team_invalid_name_left_blank():
+    """Tests exception is raised if team name is left blank"""
+
+    with pytest.raises(Exception):
+        Pokemon_Team("")
