bc1e84f452ea4d20ad22ae608446f8c59a6c6b09
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-10-25 14:29:46 +0100

    Create pokemon class and function to add pokemon to users team:

--- build_team.py
+++ build_team.py
@@ -3,6 +3,15 @@
 import re
 
 
+class Pokemon():
+    def __init__(self, name: str) -> None:
+        self._name = name
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+
 class Pokemon_Team():
     """Class for creating a team of pokemon"""
 
@@ -25,13 +34,43 @@ class Pokemon_Team():
 
         self._team_name = team_name
         self._current_team = []
+        self._current_team_max_pokemon = 4
+        self._reserve_team = []
 
     @property
     def team_name(self) -> str:
         """Returns team name"""
         return self._team_name
 
+    # TODO move edge cases to setter
     @team_name.setter
     def team_name(self, value: str) -> None:
 
         self._team_name = value
+
+    @property
+    def current_team(self) -> list:
+        """Returns list of the current team"""
+        return self._current_team
+
+    @property
+    def reserve_team(self) -> list:
+        """Returns list of the reserve team"""
+        return self._reserve_team
+
+    @property
+    def current_team_max_pokemon(self) -> list:
+        """Returns max pokemon allowed in current team"""
+        return self._current_team_max_pokemon
+
+    def add_pokemon_to_current_team(self, pokemon: Pokemon) -> None:
+        """Adds pokemon to team"""
+
+        if not isinstance(pokemon, Pokemon):
+            raise TypeError(
+                "Error: You cannot add a non-pokemon to your current team!")
+
+        if len(self.current_team) == self.current_team_max_pokemon:
+            raise Exception("Error: Your current team of pokemon is full!")
+
+        self.current_team.append(pokemon)
