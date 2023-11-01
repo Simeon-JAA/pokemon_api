d9d49bff771af901072b12fcf4d14934c43da2d0
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-11-01 14:16:56 +0000

    Add function to get user id from db

--- db_components/db_functions.py
+++ db_components/db_functions.py
@@ -2,8 +2,10 @@
 
 from os import environ
 
+import pandas as pd
 from dotenv import load_dotenv
 from psycopg2 import connect
+from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
 
 
@@ -23,11 +25,22 @@ def get_db_connection(config: dict) -> connection:
 def get_user_id(conn: connection, user_name: str) -> int:
     """Returns the user_id"""
 
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute("""SELECT user_id FROM example.api_user
+    WHERE %s = user_name;""", [user_name])
+
+    user_id = cur.fetchall()
+
+    user_id = [x['user_id'] for x in user_id]
+
+    if len(user_id) != 1:
+        raise Exception(
+            f"Error: Username '{user_name}' not found on system!")
 
     cur.close()
 
-    return
+    return user_id[0]
 
 
 if __name__ == "__main__":
@@ -37,5 +50,9 @@ if __name__ == "__main__":
     config = environ
 
     conn = get_db_connection(config)
+
+    user_id = get_user_id(conn, "example_user_1")
+
+    print(user_id)
 
     conn.close()
