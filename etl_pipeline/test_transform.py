b539aa0750b39efadc21966386a49dbff02450fe
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-11-08 11:25:47 +0000

    Tests and fixtures written for filtering flavor text entries function

--- etl_pipeline/test_transform.py
+++ etl_pipeline/test_transform.py
@@ -1,3 +1,41 @@
 """Testing file for transform"""
 
-from transform import filter_pokemon_stats
+from transform import set_abilities_language, filter_flavor_text_entries
+
+
+def test_filter_flavor_text_entries_version_group_changed_to_string(mock_flavor_text_entries_three_en_two_fr):
+    """Tests version group has been changed to string in filter_flavor_text_entries"""
+
+    result = filter_flavor_text_entries(
+        mock_flavor_text_entries_three_en_two_fr)
+
+    for item in result:
+        assert isinstance(item["version_group"], str)
+
+
+def test_filter_flavor_text_entries_version_group_correct(mock_flavor_text_entries_three_en_two_fr):
+    """Tests version group is the correct text in filter_flavor_text_entries"""
+
+    result = filter_flavor_text_entries(
+        mock_flavor_text_entries_three_en_two_fr)
+
+    for item in result:
+        assert 'mock pokemon version_group["name"]' in item["version_group"]
+
+
+def test_filter_flavor_text_entries_english_list_length(mock_flavor_text_entries_three_en_two_fr):
+    """Tests list is correct length after run through the function filter_flavor_text_entries (english)"""
+
+    result = filter_flavor_text_entries(
+        mock_flavor_text_entries_three_en_two_fr)
+
+    assert len(result) == 3
+
+
+def test_filter_flavor_text_entries_french_list_length(mock_flavor_text_entries_three_en_two_fr):
+    """Tests list is correct length after run through the function filter_flavor_text_entries (french)"""
+
+    result = filter_flavor_text_entries(
+        mock_flavor_text_entries_three_en_two_fr, 'fr')
+
+    assert len(result) == 2
