e57e77a891e43726516925b2db8daacefb326306
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-11-08 11:43:26 +0000

    Add function to filter out effect entries for a specified language

--- etl_pipeline/transform.py
+++ etl_pipeline/transform.py
@@ -17,15 +17,33 @@ def filter_flavor_text_entries(all_flavor_text_entries: list[dict], language: st
     return flavor_text_filtered
 
 
+def filter_effect_entries(all_effect_entries: list[dict], language: str = 'en') -> list[dict]:
+    """Returns the pokemon effect entries with the chosen filtered language"""
+
+    effect_entry_keys = {"effect", "short_effect"}
+
+    effect_entries_filtered = [dict((k, v) for k, v in x.items()
+                                    if k in effect_entry_keys)
+                               for x in all_effect_entries
+                               if x["language"]["name"] == language.lower()]
+
+    return effect_entries_filtered
+
+
 def set_abilities_language(pokemon_data: dict, language: str = 'en') -> dict:
     """Returns the pokemon abilities with the specified language entries only"""
 
     pokemon_abilities = pokemon_data["abilities"]
 
     for ability in pokemon_abilities:
+
         flavor_text = ability["flavor_text_entries"]
         ability["flavor_text_entries"] = filter_flavor_text_entries(
             flavor_text)
+
+        effect_entries = ability["effect_entries"]
+        ability["effect_entries"] = filter_effect_entries(effect_entries)
+
     return
 
 
