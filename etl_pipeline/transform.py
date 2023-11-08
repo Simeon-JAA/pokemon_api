38eafd065bcb491a2903c17370416ea937d5593e
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-11-08 11:24:26 +0000

    Add function to filter pokemon [abilities][flavor_text_entries] section of data

--- etl_pipeline/transform.py
+++ etl_pipeline/transform.py
@@ -1,12 +1,40 @@
 """Transform script to filter our only the data required"""
 
 
-def filter_pokemon_stats(pokemon_data: dict) -> dict:
-    """Filters out the stats section of the pokemon data"""
+def filter_flavor_text_entries(all_flavor_text_entries: list[dict], language: str = 'en') -> list[dict]:
+    """Returns the ability's flavor text for the specified language"""
 
-    keys = {"base_stat", "stat", "name"}
+    flavor_text_keys = {"flavor_text", "version_group"}
+
+    flavor_text_filtered = [dict((k, v) for k, v in x.items()
+                                 if k in flavor_text_keys)
+                            for x in all_flavor_text_entries
+                            if x["language"]["name"] == language.lower()]
+
+    for text in flavor_text_filtered:
+        text["version_group"] = text["version_group"].pop("name")
+
+    return flavor_text_filtered
+
+
+def set_abilities_language(pokemon_data: dict, language: str = 'en') -> dict:
+    """Returns the pokemon abilities with the specified language entries only"""
+
+    pokemon_abilities = pokemon_data["abilities"]
+
+    for ability in pokemon_abilities:
+        flavor_text = ability["flavor_text_entries"]
+        ability["flavor_text_entries"] = filter_flavor_text_entries(
+            flavor_text)
+    return
+
+
+def transform_pokemon_data(pokemon_data: dict) -> dict:
+    """Transforms pokemon data for loading stage"""
+
+    return pokemon_data
 
 
 if __name__ == "__main__":
 
-    filter_pokemon_stats()
+    pass
