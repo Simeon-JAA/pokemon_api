a0cd638739468a8c737dcda1b95ae33281f822dd
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-11-20 19:56:41 +0000

    Add transform_pokemon_data function to main.py

--- etl_pipeline/main.py
+++ etl_pipeline/main.py
@@ -1,7 +1,7 @@
 """ETL process"""
 
 from extract import extract_all_pokemon_urls, extract_single_pokemon
-from transform import filter_pokemon_stats
+from transform import transform_pokemon_data
 
 if __name__ == "__main__":
 
@@ -11,6 +11,8 @@ if __name__ == "__main__":
 
         pokemon_data = extract_single_pokemon(url)
 
-        print(filter_pokemon_stats(pokemon_data))
+        pokemon_data_transformed = transform_pokemon_data(pokemon_data)
+
+        print(pokemon_data)
 
         break
