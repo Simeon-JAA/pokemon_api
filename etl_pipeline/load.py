32f112db21477ff380e18d44ba7ee56b825c310d
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-11-21 11:36:21 +0000

    Create function to load pokmeon and stats into db tables

--- etl_pipeline/load.py
+++ etl_pipeline/load.py
@@ -4,6 +4,7 @@ from os import environ
 
 from dotenv import load_dotenv
 from psycopg2 import connect
+from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
 
 
@@ -20,29 +21,68 @@ def get_db_connection(config: dict) -> connection:
     return conn
 
 
-def insert_stats_into_table(conn: connection, pokemon_data: dict) -> None:
-    """Inserts pokemon stat data into the database"""
+def insert_into_pokemon_table(conn: connection, pokemon_data: dict) -> int:
+    """Inserts pokemon into pokemon table
+    RETURNS POKEMON ID!!"""
 
-    stats = pokemon_data["stats"]
+    pokemon_name = pokemon_data["name"]
+
+    cur = conn.cursor(cursor_factory=RealDictCursor)
 
     try:
-        cur = conn.cursor()
+        cur.execute(f"""INSERT INTO pokemon
+                (pokemon_name)
+                VALUES
+                (%s) RETURNING*;""",
+                    [pokemon_name])
+    except:
+        ConnectionError("Error: Connection to insert data unsuccessful!")
 
-        cur.execute("""INSERT INTO
-                    health, attack, defense, special_attack, 
-                    special_defense, speed, height, weight
-                    VALUES 
-                    (%s, %s, %s, %s, %s, %s, %s, %s);""",
-                    [stats["health"], stats["attack"], stats["defense"],
-                     stats["special-attack"], stats["special-defense"], stats["speed"],
-                     stats["height"], stats["weight"]])
+    pokemon_insert_data = cur.fetchall()
 
-        conn.commit()
-
-    except (ConnectionError, ConnectionAbortedError) as conn_err:
-        conn_err("Error: Unable to successfully input data into database!")
+    pokemon_id = pokemon_insert_data[0]["pokemon_id"]
 
     cur.close()
+
+    conn.commit()
+
+    return pokemon_id
+
+
+def insert_into_pokemon_stats_table(conn: connection, pokemon_data: dict, pokemon_id: int) -> None:
+    """Inserts into pokemon_stats table"""
+
+    pokemon_stats = pokemon_data["stats"]
+
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    try:
+        cur.execute("""INSERT INTO pokemon_stats
+                (pokemon_id, hp, attack, defense, speed, 
+                special_attack, special_defense, height, pokemon_weight)
+                VALUES
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s)RETURNING*;""",
+                    [pokemon_id, pokemon_stats["hp"], pokemon_stats["attack"],
+                     pokemon_stats["defense"], pokemon_stats["speed"], pokemon_stats["special-attack"],
+                     pokemon_stats["special-defense"], pokemon_stats["height"], pokemon_stats["weight"]])
+    except:
+        ConnectionError(
+            "Error: Unsuccessful connection attempt with the database!")
+
+    cur.close()
+
+    conn.commit()
+
+
+def load_pokemon_into_db(conn: connection, pokemon_data: dict) -> None:
+    """Loads one pokemon into the postgres database"""
+
+    try:
+        pokemon_id = insert_into_pokemon_table(conn, pokemon_data)
+        insert_into_pokemon_stats_table(conn, pokemon_data, pokemon_id)
+
+    except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError) as conn_err:
+        conn_err("Error: Connection unsuccessful with the database!")
 
     return
 
