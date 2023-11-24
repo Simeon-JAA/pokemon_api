fa6295a06927d3e6daa0a4ffa16ea6b5fec92faa
Simeon Adesanya <simeon_a@simeons-mbp.home>
2023-11-24 15:34:23 +0000

    Add time function to display time process takes

--- etl_pipeline/main.py
+++ etl_pipeline/main.py
@@ -1,5 +1,6 @@
 """ETL process"""
 
+import time
 from os import environ
 
 from dotenv import load_dotenv
@@ -18,6 +19,8 @@ if __name__ == "__main__":
 
     pokemon_urls = extract_all_pokemon_urls()
 
+    start_time = time.time()
+
     for url in pokemon_urls:
 
         pokemon_data = extract_single_pokemon(url)
@@ -25,5 +28,11 @@ if __name__ == "__main__":
         pokemon_data_transformed = transform_pokemon_data(pokemon_data)
 
         load_pokemon_into_db(conn, pokemon_data)
+
+        load_time = time.time()
+
+        pokemon_name = pokemon_data_transformed["name"]
+
+        print(f"{pokemon_name} added to database! {load_time - start_time}s ...")
 
     conn.close()
