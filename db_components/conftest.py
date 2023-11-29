e090f893cf81f3e86348fcbfb29a6fd0afbbef54
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-11-29 16:42:37 +0000

    Add pytest fixture to mock pokemon types

--- db_components/conftest.py
+++ db_components/conftest.py
@@ -17,7 +17,7 @@ def mock_config_environment() -> dict:
 
 
 @pytest.fixture
-def mock_all_pokemon_names() -> list[str]:
+def mock_all_pokemon_nanmes() -> list[str]:
     """Returns mock of mocked pokemon names"""
 
     return ["mock_pokemon_name_1",
@@ -28,3 +28,18 @@ def mock_all_pokemon_names() -> list[str]:
             "mock_pokemon_name_6",
             "mock_pokemon_name_n"
             ]
+
+
+@pytest.fixture
+def mock_all_pokemon_types() -> list[str]:
+    """Mocks and returns all types"""
+
+    return [
+        "mock_type_1",
+        "mock_type_2",
+        "mock_type_3",
+        "mock_type_4",
+        "mock_type_5",
+        "mock_type_6",
+        "mock_type_n",
+    ]
