c160d3920745fea6adcb1c340df9aa540803206b
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-12-02 07:58:31 +0000

    add function and query to get all abilities

--- db_components/db_queries.py
+++ db_components/db_queries.py
@@ -60,6 +60,25 @@ def get_all_pokemon_types(db_conn: connection) -> list[str]:
     return all_pokemon_types_df["types"].to_list()
 
 
+def get_all_pokemon_abilities(db_conn: connection) -> list[str]:
+    """Returns list of all pokemon abilities in the database"""
+
+    cur = db_conn.cursor(cursor_factory=RealDictCursor)
+
+    try:
+        cur.execute("""SELECT DISTINCT(LOWER(ability_name)) AS ability_name
+                FROM pokemon_ability;""")
+
+    except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError) as conn_err:
+        raise conn_err("Error: Error with db connection!")
+
+    all_pokemon_abilities = cur.fetchall()
+    cur.close()
+    all_pokemon_abilities_df = pd.DataFrame(all_pokemon_abilities)
+
+    return all_pokemon_abilities_df["ability_name"].to_list()
+
+
 def get_all_pokemon_moves(db_conn: connection) -> list[str]:
     """Returns list of all pokemon moves in the database"""
 
@@ -236,7 +255,8 @@ def get_all_pokemon_move_names(db_conn: connection) -> DataFrame:
     return pokemon_data_df
 
 
-# TODO add list support option
+# If (move_1, move_2, move_3) and input is (move_1, move_3) pokemon will not show up
+# TODO fix this
 def get_pokemon_by_move_name(db_conn: connection, pokemon_move: str | list[str]) -> DataFrame:
     """Return data frame of pokemon that are associated with the move"""
 
@@ -351,6 +371,47 @@ def get_pokemon_by_type(db_conn: connection, pokemon_type: str | list[str]) -> D
     return pokemon_by_type_df
 
 
+# If (ability_1, ability_2, ability_3) and input is (ability_1, ability_3) pokemon will not show up
+# TODO fix this
+def get_pokemon_by_ability(db_conn: connection, pokemon_ability: str | list[str]) -> DataFrame:
+    """Returns all pokemon of a specific type"""
+
+    all_pokemon_abilities = ge(db_conn)
+
+    if isinstance(pokemon_ability, str):
+        if pokemon_ability.lower() not in all_pokemon_types:
+            raise ValueError("Error: Pokemon type not in database!")
+
+    elif isinstance(pokemon_ability, list):
+        for p_type in pokemon_ability:
+
+            if not isinstance(p_type, str):
+                raise TypeError(f"Error: {p_type} should be of a string type!")
+
+            if p_type.lower() not in all_pokemon_types:
+                raise ValueError(
+                    f"Error: Pokemon type {p_type} not in database!")
+
+        pokemon_ability.sort()
+        pokemon_ability = ", ".join(pokemon_ability)
+
+    cur = db_conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute("""SELECT p.pokemon_id, pokemon_name,
+                STRING_AGG (pokemon_type, ', ' ORDER BY pokemon_type) AS pokemon_types
+                FROM pokemon AS p
+                JOIN pokemon_types AS pt
+                ON p.pokemon_id = pt.pokemon_id
+                GROUP BY p.pokemon_id
+                HAVING STRING_AGG (pokemon_type, ', ' ORDER BY pokemon_type) ILIKE %s;""",
+                [f"%{pokemon_ability.title()}%"])
+
+    pokemon_by_type = cur.fetchall()
+    pokemon_by_type_df = pd.DataFrame(pokemon_by_type)
+
+    return pokemon_by_type_df
+
+
 # TODO version_group_control count
 def specific_version_control_count(db_conn: connection, move_name: str) -> DataFrame:
     """Return version control for move specified"""
@@ -390,6 +451,7 @@ if __name__ == "__main__":
     # all_pokemon_names = get_all_pokemon_names(conn)
     # all_pokemon_types = get_all_pokemon_types(conn)
     # all_pokemon_moves = get_all_pokemon_moves(conn)
+g    # all_pokemon_abilities = get_all_pokemon_abilities(conn)
 
     # print(get_all_pokemon(conn))
     # print(get_specific_pokemon(conn, 'Bulbasaur'))
@@ -399,7 +461,8 @@ if __name__ == "__main__":
     # print(get_pokemon_by_type(conn, ["rock", "fire"]))
 
     # -- This code is testing
-    print(get_pokemon_by_move_name(conn, ["Aerial Ace", "Agility"]))
+    # print(get_pokemon_by_move_name(conn, ["Aerial Ace", "Agility"]))
+    # print(get_pokemon_by_ability(conn, "Overgrow"))
     # print(get_specific_pokemon_moves(conn, "bulbasaur"))
     # print(version_control_count(conn))
 
